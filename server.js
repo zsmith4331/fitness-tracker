b847238e471fbe818961618ebd9cdd953c235d66
Zach Smith <zsmith4331@gmail.com>
2021-04-02 20:16:27 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+// Dependencies //
 const express = require("express");
 const logger = require("morgan"); 
 const mongoose = require("mongoose");
 
+// Set the port application //
 const PORT = process.env.PORT || 8080
 
+// Create express app instance //
 const app = express();
 
+// Parse application body as JSON //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static content for the app //
 app.use(express.static("public"));
 
+// Requiried Routes //
+require("./public/routes/html")(app);
+require("./public/routes/api")(app);
+
+// Connecting to MongoDb //
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-app.use(require("./routes/api.js"));
-
+// Start server to listen to client requests //
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
