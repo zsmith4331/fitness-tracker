0a9957a0ce5280355633e0aafd0984511663b95f
Zach Smith <zsmith4331@gmail.com>
2021-04-02 21:49:18 -0400

    added models

---
+++ models/workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for transaction"
+    },
+    value: {
+      type: Number,
+      required: "Enter an amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+  const workout = mongoose.model("Transaction", workoutSchema);
+  
+  module.exports = workout;
+  
